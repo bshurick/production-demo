43c26de557027b44d1c04ac3f54f6d36da5aba33
Brandon Shurick <brandon.shurick@gmail.com>
2022-03-22 15:46:33 -0700

    update notebook

--- src/production_demo/__init__.py
+++ src/production_demo/__init__.py
@@ -1,2 +1,2 @@
 from production_demo.train import CategoriesTransformer
-from production_demo.constants import CATEGORIES, NUMERICS
+from production_demo.constants import CATEGORIES, NUMERICS, OUTPUT
