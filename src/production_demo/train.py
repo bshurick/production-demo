66b297680898dc30255b54765e02ac340cb8d371
Brandon Shurick <brandon.shurick@gmail.com>
2022-03-22 19:30:30 -0700

    move model params to constants

--- src/production_demo/train.py
+++ src/production_demo/train.py
@@ -43,18 +43,9 @@ def handler():
     hct = CategoriesTransformer(CATEGORIES)
     model = Pipeline([
       ('hash', hct),
-      ('LGBM', LGBMRegressor(**MODEL_PARAMS)),
+      ('LGBM', LGBMRegressor()),
     ])
-    model.set_params(**{'LGBM__colsample_bytree': 0.4569053964451767, 
-                        'LGBM__learning_rate': 0.060215823699935396, 
-                        'LGBM__max_depth': 10, 
-                        'LGBM__min_child_weight': 0.06494958890542044, 
-                        'LGBM__min_split_gain': 0.05643039388402624, 
-                        'LGBM__n_estimators': 354, 
-                        'LGBM__num_leaves': 3192, 
-                        'LGBM__reg_alpha': 260.8146685873852, 
-                        'LGBM__reg_lambda': 101.43819465504578, 
-                        'LGBM__subsample': 0.4382540932521588})
+    model.set_params(**MODEL_PARAMS)
 
     # train model
     model.fit(train[NUMERICS + CATEGORIES], train[OUTPUT])
