450fe70d7d42d768bce120354db396592a007c4f
Brandon Shurick <brandon.shurick@gmail.com>
2022-03-22 21:07:26 -0700

    run black

--- test/test_handler.py
+++ test/test_handler.py
@@ -6,10 +6,7 @@ from sklearn.pipeline import Pipeline
 
 from production_demo import train
 from production_demo.train import handler
-from production_demo.constants import (CATEGORIES, 
-                                       NUMERICS, 
-                                       MODEL_PARAMS, 
-                                       OUTPUT)
+from production_demo.constants import CATEGORIES, NUMERICS, MODEL_PARAMS, OUTPUT
 
 from unittest.mock import MagicMock, call, ANY
 
@@ -19,34 +16,32 @@ def test_train_handler(monkeypatch):
     mock_pandas = MagicMock()
     mock_pipeline = MagicMock()
     mock_dump = MagicMock()
-    monkeypatch.setattr(pd, 'read_csv', mock_pandas)
-    monkeypatch.setattr(train, 'Pipeline', mock_pipeline)
-    monkeypatch.setattr(train, 'dump', mock_dump)
+    monkeypatch.setattr(pd, "read_csv", mock_pandas)
+    monkeypatch.setattr(train, "Pipeline", mock_pipeline)
+    monkeypatch.setattr(train, "dump", mock_dump)
 
     # WHEN
     handler()
 
-    # THEN 
+    # THEN
     assert mock_pandas.mock_calls == [
-        # read CSV 
+        # read CSV
         call(ANY),
-
-        # get X 
+        # get X
         call().__getitem__(NUMERICS + CATEGORIES),
-
         # get y
         call().__getitem__(OUTPUT),
     ]
     assert mock_pipeline.mock_calls == [
-        # create pipeline 
+        # create pipeline
         call(ANY),
-
-        # set parameters 
-        call().set_params(**MODEL_PARAMS), 
-
+        # set parameters
+        call().set_params(**MODEL_PARAMS),
         # fit x, y
-        call().fit(mock_pandas().__getitem__(NUMERICS + CATEGORIES), 
-                   mock_pandas().__getitem__(OUTPUT)),
+        call().fit(
+            mock_pandas().__getitem__(NUMERICS + CATEGORIES),
+            mock_pandas().__getitem__(OUTPUT),
+        ),
     ]
     assert mock_dump.mock_calls == [
         # dump model artifacts
