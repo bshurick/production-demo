91916c372c5f93039a9f490c70e1ae8f933e4a3e
Brandon Shurick <brandon.shurick@gmail.com>
2022-03-26 13:51:45 -0700

    use flask

--- src/production_demo/service.py
+++ src/production_demo/service.py
@@ -10,12 +10,18 @@ https://github.com/aws/sagemaker-inference-toolkit
 
 import os
 
-from sagemaker_inference import default_inference_handler
+from flask import Flask, request
+from sagemaker_inference import (
+    decoder,
+    encoder,
+)
+from subprocess import Popen
+
 from joblib import load
 from production_demo.constants import TRAINED_MODEL_NAME
 
 
-class InferenceHandler(default_inference_handler.DefaultInferenceHandler):
+class InferenceHandler:
     """Default handler for model inference
 
     This class implements `default_model_fn` and `default_predict_fn`.
@@ -54,3 +60,45 @@ class InferenceHandler(default_inference_handler.DefaultInferenceHandler):
         :rtype: pandas.DataFrame
         """
         return model.predict(data)
+
+    def input_fn(self, input_data, content_type):
+        """A default input_fn that can handle JSON, CSV and NPZ formats.
+
+        Args:
+            input_data: the request payload serialized in the content_type format
+            content_type: the request content_type
+
+        Returns: input_data deserialized into torch.FloatTensor or torch.cuda.FloatTensor depending if cuda is available.
+        """
+        return decoder.decode(input_data, content_type)
+
+    def output_fn(self, prediction, accept):
+        """A default output_fn for PyTorch. Serializes predictions from predict_fn to JSON, CSV or NPY format.
+
+        Args:
+            prediction: a prediction result from predict_fn
+            accept: type which the output data needs to be serialized
+
+        Returns: output data serialized
+        """
+        return encoder.encode(prediction, accept)
+
+
+app = None
+def start_server():
+    global app
+    app = Flask(__name__)
+    handler = InferenceHandler()
+    model = handler.model_fn('/opt/ml/model')
+
+    @app.route("/invocations", methods=["POST"])
+    def invoke(input_data):
+        data = handler.input_fn(input_data=input_data,
+                                content_type=request.content_type)
+        prediction = handler.predict_fn(data, model)
+        output = handler.output_fn(prediction, "CSV")
+        return output
+
+
+def main():
+    Popen(["gunicorn", "-w", "4", "production_demo.service:start_server"])
