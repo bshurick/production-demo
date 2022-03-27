2e7366bdd469c02f751ee026b90c6a4a59b787ad
Brandon Shurick <brandon.shurick@gmail.com>
2022-03-26 17:05:25 -0700

    add content to test

--- test_integ/test_local_endpoint.py
+++ test_integ/test_local_endpoint.py
@@ -5,7 +5,24 @@ import pandas as pd
 import requests
 from io import StringIO
 
-TEST_REQUEST = '{"1stFlrSF":896,"2ndFlrSF":0,"BedroomAbvGr":2,"EnclosedPorch":0,"Fireplaces":0,"FullBath":1,"GarageArea":730.0,"GarageCars":1.0,"GrLivArea":896,"HalfBath":0,"KitchenAbvGr":1,"LotArea":11622,"OpenPorchSF":0,"OverallCond":6,"OverallQual":5,"PoolArea":0,"TotRmsAbvGrd":5,"TotalBsmtSF":882.0,"WoodDeckSF":140,"YearBuilt":1961,"YearRemodAdd":1961,"BldgType":"1Fam","CentralAir":"Y","Electrical":"SBrkr","ExterCond":"TA","ExterQual":"TA","Fence":"MnPrv","FireplaceQu":null,"Foundation":"CBlock","Functional":"Typ","GarageCond":"TA","GarageQual":"TA","GarageType":"Attchd","Heating":"GasA","HeatingQC":"TA","HouseStyle":"1Story","KitchenQual":"TA","LotConfig":"Inside","MasVnrType":"None","MSSubClass":20,"PavedDrive":"Y","RoofStyle":"Gable"}\n'
+
+TEST_REQUEST = (
+    '{"1stFlrSF":896,"2ndFlrSF":0,"BedroomAbvGr":2,'
+    '"EnclosedPorch":0,"Fireplaces":0,"FullBath":1,'
+    '"GarageArea":730.0,"GarageCars":1.0,"GrLivArea"'
+    ':896,"HalfBath":0,"KitchenAbvGr":1,"LotArea":11622'
+    ',"OpenPorchSF":0,"OverallCond":6,"OverallQual":5,'
+    '"PoolArea":0,"TotRmsAbvGrd":5,"TotalBsmtSF":882.0,'
+    '"WoodDeckSF":140,"YearBuilt":1961,"YearRemodAdd":'
+    '1961,"BldgType":"1Fam","CentralAir":"Y","Electrical"'
+    ':"SBrkr","ExterCond":"TA","ExterQual":"TA","Fence":'
+    '"MnPrv","FireplaceQu":null,"Foundation":"CBlock",'
+    '"Functional":"Typ","GarageCond":"TA","GarageQual":'
+    '"TA","GarageType":"Attchd","Heating":"GasA","HeatingQC"'
+    ':"TA","HouseStyle":"1Story","KitchenQual":"TA",'
+    '"LotConfig":"Inside","MasVnrType":"None","MSSubClass":20,'
+    '"PavedDrive":"Y","RoofStyle":"Gable"}\n'
+)
 
 
 def test_endpoint():
@@ -14,4 +31,4 @@ def test_endpoint():
         json=TEST_REQUEST,
         headers={"Content-Type": "application/json"},
     )
-    assert result.content == ""
+    assert result.content == "150000"
